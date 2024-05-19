97ec2da96a51f5dfbacaf84f5ba6997aa7f2ba62
EloyAlbiach <eloy.albiach@gmail.com>
2024-05-19 20:47:28 +0200

    Pruebas SonarCloud III add secrets and sonar_project_properties V

--- tests/DniTest.php
+++ tests/DniTest.php
@@ -9,5 +9,11 @@ class DniTest extends TestCase{
         $result = $calcDNI->prueba("19066557");
         $this->assertEquals("V", $result);
     }
+
+    public function testII(){
+        $calcDNI = new Funcionalidades\CalcularDni();
+        $result = $calcDNI->prueba("12455789");
+        $this->assertEquals("R", $result);
+    }
 }
 ?>
